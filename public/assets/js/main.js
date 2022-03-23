e1e5a7c16bcf6a308d5475f0f0dd44a532b9de20
GemaMesasV <gema.mesas.v@gmail.com>
2022-03-23 13:44:06 +0100

    session 4 changes

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
-console.log('>> Ready :)');
+// console.log('>> Ready :)');
+
+const designLegend = document.querySelector ('.js__design-legend');
+
+const designFieldset = document.querySelector ('.js__design-fieldset');
+
+designLegend.addEventListener ("click", (event) => {
+
+    designFieldset.classList.toggle (".collapsed");
+});
+
 
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('>> Ready :)');
+// console.log('>> Ready :)');
+
+const designLegend = document.querySelector ('.js__design-legend');
+
+const designFieldset = document.querySelector ('.js__design-fieldset');
+
+designLegend.addEventListener ("click", (event) => {
+
+    designFieldset.classList.toggle (".collapsed");
+});
+
