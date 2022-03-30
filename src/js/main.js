7f1924238b597c26d7f36350f78a49bf764c7053
Analía Río <analia.rio@gmail.com>
2022-03-30 21:57:22 +0200

    JS Event Listeners for Profile Color Change

--- src/js/main.js
+++ src/js/main.js
@@ -55,3 +55,17 @@ shareLegend.addEventListener('click', () => {
 // const radioButton1 = document.querySelector(".js-radioButton1");
 // const radioButton2 = document.querySelector(".js-radioButton2");
 // const radioButton3 = document.querySelector(".js-radioButton3");
+
+// EVENT LISTENERS FOR PROFILE COLOR CHANGE
+
+// radioButton1.addEventListener('click', () => {
+//   --- code with instructions ---
+// });
+
+// radioButton2.addEventListener('click', () => {
+//   --- code with instructions ---
+// });
+
+// radioButton3.addEventListener('click', () => {
+//   --- code with instructions ---
+// });
