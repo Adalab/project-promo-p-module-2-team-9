52c29f59f0f1501e0d4aa1373ff1202d957d8bf7
Analía Río <analia.rio@gmail.com>
2022-03-30 21:50:30 +0200

    Constants for profile color change with JS

--- src/js/main.js
+++ src/js/main.js
@@ -2,39 +2,22 @@
 
 // console.log('>> Ready :)');
 
-// COLAPSABLE DISEÑA
-
+// CONSTANTS FOR DESIGN COLLAPSIBLE
 const designLegend = document.querySelector('.js__design-legend');
 const designColors = document.querySelector('.js__design-colors');
 const arrowDown1 = document.querySelector('.js__arrowDown1');
 
-// designLegend.addEventListener('click', () => {
-//   designColors.classList.toggle('collapsed');
-//   arrowDown1.classList.toggle('rotate');
-// });
-
-
-// COLAPSABLE RELLENA
-
+// CONSTANTS FOR FILL COLLAPSIBLE
 const fillLegend = document.querySelector('.js__fill-legend');
 const fillForm = document.querySelector('.js__field');
 const arrowDown2 = document.querySelector('.js__arrowDown2');
 
-// fillLegend.addEventListener('click', () => {
-//   fillForm.classList.toggle('collapsed');
-//   arrowDown2.classList.toggle('rotate');
-//   designColors.classList.add('hidden');
-
-// });
-
-
-
-//COLAPSABLE SHARE
-
+// CONSTANTS FOR SHARE COLLAPSIBLE
 const shareLegend = document.querySelector('.js__share-legend');
 const sharecontent = document.querySelector('.js__share');
 const arrowDown3 = document.querySelector('.js__arrowDown3');
 
+// DESIGN COLLAPSIBLE
 designLegend.addEventListener('click', () => {
   designColors.classList.toggle('collapsed');
   arrowDown1.classList.toggle('rotate');
@@ -44,7 +27,7 @@ designLegend.addEventListener('click', () => {
   fillForm.classList.add('collapsed');
 });
 
-
+// FILL COLLAPSIBLE
 fillLegend.addEventListener('click', () => {
   fillForm.classList.toggle('collapsed');
   arrowDown2.classList.toggle('rotate');
@@ -54,7 +37,7 @@ fillLegend.addEventListener('click', () => {
   sharecontent.classList.add('collapsed');
 });
 
-
+// SHARE COLLAPSIBLE
 shareLegend.addEventListener('click', () => {
   sharecontent.classList.toggle('collapsed');
   arrowDown3.classList.toggle('rotate');
@@ -63,3 +46,12 @@ shareLegend.addEventListener('click', () => {
   designColors.classList.add('collapsed');
   fillForm.classList.add('collapsed');
 });
+
+
+// CONSTANTS FOR PROFILE COLOR CHANGE
+// const profileName = document.querySelector(".js-profileName");
+// const profileBorder = document.querySelector(".js-profileBorder");
+// const profileIcon = document.querySelector(".js-profileIcon");
+// const radioButton1 = document.querySelector(".js-radioButton1");
+// const radioButton2 = document.querySelector(".js-radioButton2");
+// const radioButton3 = document.querySelector(".js-radioButton3");
