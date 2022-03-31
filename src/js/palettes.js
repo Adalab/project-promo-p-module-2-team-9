0aefe8deb009fa6ee76da268184ec760c595384b
Iris Mireya <irismzb_86@hotmail.com>
2022-03-31 13:38:29 +0200

    reset btn

---
+++ src/js/palettes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// CONSTANTS FOR PROFILE COLOR CHANGE
+// const profileName = document.querySelector(".js-profileName");
+// const profileBorder = document.querySelector(".js-profileBorder");
+// const profileIcon = document.querySelector(".js-profileIcon");
+// const radioButton1 = document.querySelector(".js-radioButton1");
+// const radioButton2 = document.querySelector(".js-radioButton2");
+// const radioButton3 = document.querySelector(".js-radioButton3");
+
+// EVENT LISTENERS FOR PROFILE COLOR CHANGE
+
+// radioButton1.addEventListener('click', () => {
+//   --- code with instructions ---
+// });
+
+// radioButton2.addEventListener('click', () => {
+//   --- code with instructions ---
+// });
+
+// radioButton3.addEventListener('click', () => {
+//   --- code with instructions ---
+// });
---
+++ src/js/profile.js
@@ -0,0 +1,18 @@
+'use strict';
+//4. Cancelar formulario
+const buttonCancelForm = document.querySelector('.js-btn-cancel');
+const reset = document.querySelector('.js-reset');
+function handleClickResetBtn(event) {
+    event.preventDefault();
+    reset.value = '';
+}
+
+buttonCancelForm.addEventListener('click', (event) => {
+    event.preventDefault();
+    const allResetInputs = document.querySelectorAll('.js-reset');
+    for(const eachResetInput of allResetInputs) {
+    eachResetInput.value = '';
+}
+});
+
+
