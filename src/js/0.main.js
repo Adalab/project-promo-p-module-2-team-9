9889ff4ccf12e25fadce75174d35a3909cfe26be
GemaMesasV <gema.mesas.v@gmail.com>
2022-04-14 13:39:42 +0200

    change example photo

--- src/js/0.main.js
+++ src/js/0.main.js
@@ -1,22 +1,29 @@
 'use strict';
 
+// CONSTANTS FOR DESIGN COLLAPSIBLE
 const designLegend = document.querySelector('.js__design-legend');
 const designColors = document.querySelector('.js__design-colors');
 const arrowDown1 = document.querySelector('.js__arrowDown1');
+// CONSTANTS FOR FILL COLLAPSIBLE
 const fillLegend = document.querySelector('.js__fill-legend');
 const fillForm = document.querySelector('.js__field');
 const arrowDown2 = document.querySelector('.js__arrowDown2');
+// CONSTANTS FOR SHARE COLLAPSIBLE
 const shareLegend = document.querySelector('.js__share-legend');
 const sharecontent = document.querySelector('.js__share');
 const arrowDown3 = document.querySelector('.js__arrowDown3');
+// CONSTANTS FOR PALETTES
 const allRadioButtons = document.querySelectorAll('.js-radio');
 const previewContainer = document.querySelector('.js-preview');
+//4. RESET BUTTON
 const buttonCancelForm = document.querySelector('.js-btn-cancel');
 const reset = document.querySelector('.js-reset');
+// 5. Variables avatar
 const fr = new FileReader();
 const fileField = document.querySelector('.js__profile-upload-btn');
 const profileImage = document.querySelector('.js__profile-image');
 const profilePreview = document.querySelector('.js__profile-preview');
+
 const inputs = document.querySelector('.js__inputs');
 const previewNameElement = document.querySelector('.js_name');
 const previewJobElement = document.querySelector('.js_job');
@@ -24,6 +31,7 @@ const previewEmailElement = document.querySelector('.js_email');
 const previewPhoneElement = document.querySelector('.js_phone');
 const previewLinkedinElement = document.querySelector('.js_linkedin');
 const previewGithubElement = document.querySelector('.js_github');
+
 const btnShare = document.querySelector('.js-share-btn');
 const twitterBtn = document.querySelector('.js-twitter-button');
 let msg = document.querySelector('.js-message');
@@ -39,7 +47,7 @@ let data = {
   github: '',
   photo: '',
 };
-
+// DESIGN COLLAPSIBLE
 designLegend.addEventListener('click', () => {
   designColors.classList.toggle('collapsed');
   arrowDown1.classList.toggle('rotate');
@@ -47,11 +55,14 @@ designLegend.addEventListener('click', () => {
   arrowDown3.classList.remove('rotate');
   sharecontent.classList.add('collapsed');
   fillForm.classList.add('collapsed');
+
+  //Cuando falta algun dato en el formulario, volvemos a esconder la seccion del boton para que aparezcan los estilos por defecto
   textCard.innerHTML = '';
   msg.classList.add('hidden');
   btnShare.classList.remove('white');
 });
 
+// FILL COLLAPSIBLE
 fillLegend.addEventListener('click', () => {
   fillForm.classList.toggle('collapsed');
   arrowDown2.classList.toggle('rotate');
@@ -59,11 +70,14 @@ fillLegend.addEventListener('click', () => {
   arrowDown3.classList.remove('rotate');
   designColors.classList.add('collapsed');
   sharecontent.classList.add('collapsed');
+
+  //Cuando falta algun dato en el formulario, volvemos a esconder la seccion del boton para que aparezcan los estilos por defecto
   textCard.innerHTML = '';
   msg.classList.add('hidden');
   btnShare.classList.remove('white');
 });
 
+// SHARE COLLAPSIBLE
 shareLegend.addEventListener('click', () => {
   sharecontent.classList.toggle('collapsed');
   arrowDown3.classList.toggle('rotate');
@@ -73,6 +87,7 @@ shareLegend.addEventListener('click', () => {
   fillForm.classList.add('collapsed');
 });
 
+// HANDLING FUNCTION
 function handleClickRadioUnique(event) {
   const paletteClassToAdd = `palette-${event.currentTarget.value}`;
   data.palette = event.currentTarget.value;
@@ -82,11 +97,14 @@ function handleClickRadioUnique(event) {
   previewContainer.classList.add(paletteClassToAdd);
 }
 
+// LOOP
 for (const eachRadioButton of allRadioButtons) {
   eachRadioButton.addEventListener('click', handleClickRadioUnique);
 }
 
 function renderPreview() {
+  //previewNameElement.innerHTML = data.name;
+  //previewJobElement.innerHTML = data.job;
   previewEmailElement.href = `mailto: ${data.email}`;
   previewPhoneElement.href = `tel: ${data.phone}`;
   previewLinkedinElement.href = data.linkedin;
@@ -122,6 +140,7 @@ function handleKeyupInputs(event) {
   renderPreview();
 }
 
+//6.reset
 buttonCancelForm.addEventListener('click', (event) => {
   event.preventDefault();
   previewContainer.classList.add('palette-1');
@@ -140,6 +159,7 @@ buttonCancelForm.addEventListener('click', (event) => {
     profilePreview.style.backgroundImage = '';
   }
 });
+//7.share
 
 function handleClickCreateBtn(event) {
   event.preventDefault();
@@ -148,9 +168,9 @@ function handleClickCreateBtn(event) {
   msg.classList.remove('hidden');
   btnShare.classList.add('white');
   fetch('https://awesome-profile-cards.herokuapp.com/card', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data),
+    method: 'POST', // enviar datos a la API
+    headers: { 'Content-Type': 'application/json' }, //tipo de dato
+    body: JSON.stringify(data), //datos que de quiero enviar, en este caso es data, pasandolo a string
   })
     .then((response) => response.json())
     .then((serverResp) => {
@@ -172,23 +192,53 @@ function handleClickCreateBtn(event) {
   });
 }
 
+//Eventos
 inputs.addEventListener('keyup', handleKeyupInputs);
 btnShare.addEventListener('click', handleClickCreateBtn);
 
+//Incluir foto de avatar
+
+/**
+ * Recoge el archivo añadido al campo de tipo "file"
+ * y lo carga en nuestro objeto FileReader para que
+ * lo convierta a algo con lo que podamos trabajar.
+ * Añade un listener al FR para que ejecute una función
+ * al tener los datos listos
+ * @param {evento} e
+ */
 function getImage(e) {
   const myFile = e.currentTarget.files[0];
   fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
 }
 
+/**
+ * Una vez tenemos los datos listos en el FR podemos
+ * trabajar con ellos ;)
+ */
 function writeImage() {
+  /* En la propiedad `result` de nuestro FR se almacena
+   * el resultado. Ese resultado de procesar el fichero que hemos cargado
+   * podemos pasarlo como background a la imagen de perfil y a la vista previa
+   * de nuestro componente.
+   */
   profileImage.style.backgroundImage = `url(${fr.result})`;
   profilePreview.style.backgroundImage = `url(${fr.result})`;
   data.photo = fr.result;
 }
 
+/**
+ * Genera un click automático en nuesto campo de tipo "file"
+ * que está oculto
+ */
 function fakeFileClick() {
   fileField.click();
 }
 
+/**
+ * Añadimos los listeners necesarios:
+ * - al botón visible para generar el click automático
+ * - al campo oculto para cuando cambie su value
+ */
 fileField.addEventListener('change', getImage);
+
